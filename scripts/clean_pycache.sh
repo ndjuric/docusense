e9e35eb24dca6c86c88a6786695abe78e0aad62b
ndjuric <4016107+ndjuric@users.noreply.github.com>
2025-02-06 22:01:00 +0100

    add script to clean pycache files

---
+++ scripts/clean_pycache.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+find . -type d -name "__pycache__" -exec rm -rf {} +
+find . -type d -name ".pytest_cache" -exec rm -rf {} +
+find . -type d -name ".mypy_cache" -exec rm -rf {} +
+find . -type f -name "*.pyc" -exec rm -f {} +
+find . -type f -name "*.pyo" -exec rm -f {} +
+find . -type d -name "*.egg-info" -exec rm -rf {} +
+rm -rf build dist
+
+echo "Clean complete."
